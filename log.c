83f9a319296334ba99e187b42ca003fb5d93ad4c
Joe Reid <joe@joereid.com>
2019-09-22 22:13:13 -0400

    while this works, I see some strange values in the db

--- log.c
+++ log.c
@@ -9,14 +9,14 @@ void LOG_Chat_f(q2_server_t *srv)
 {
 	char *sql;
 	MYSQL_STMT *st;
-	MYSQL_BIND params[2];
-	uint8_t cl;
+	MYSQL_BIND params[4];
+	uint8_t cl, pl;
 	char *str;
 
 	cl = MSG_ReadByte();
 	str = MSG_ReadString();
 
-	sql = "INSERT INTO chat (server, chat_date, message) VALUES (?, NOW(), ?)";
+	sql = "INSERT INTO chat (server, client_num, player_id, chat_date, message) VALUES (?, ?, ?, NOW(), ?)";
 
 	st = mysql_stmt_init(srv->db);
 
@@ -24,14 +24,22 @@ void LOG_Chat_f(q2_server_t *srv)
 		printf("%s\n", mysql_stmt_error(st));
 	}
 
+	pl = 0;	// for now player_id is just 0
+
 	memset(&params, 0x0, sizeof(params));
 
 	params[0].buffer_type = MYSQL_TYPE_LONG;
 	params[0].buffer = (char *) &srv->id;
 
-	params[1].buffer_type = MYSQL_TYPE_STRING;
-	params[1].buffer = str;
-	params[1].buffer_length = strlen(str);
+	params[1].buffer_type = MYSQL_TYPE_LONG;
+	params[1].buffer = (char *) &cl;
+
+	params[2].buffer_type = MYSQL_TYPE_LONG;
+	params[2].buffer = (char *) &pl;
+
+	params[3].buffer_type = MYSQL_TYPE_STRING;
+	params[3].buffer = str;
+	params[3].buffer_length = strlen(str);
 
 	if (mysql_stmt_bind_param(st, params)) {
 		printf("%s\n", mysql_stmt_error(st));
