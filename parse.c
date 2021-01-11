cd03fadaf31b5eecfc81e763d5dbd0c991f00fb5
Joe Reid <joe@joereid.com>
2021-01-11 11:54:10 -0500

    tab to spaces

--- parse.c
+++ parse.c
@@ -65,12 +65,12 @@ void ParseMessage(q2_server_t *q2, msg_buffer_t *msg)
  */
 void ParseHello(hello_t *h, msg_buffer_t *in)
 {
-	h->key = MSG_ReadLong(in);
-	h->version = MSG_ReadLong(in);
-	h->port = MSG_ReadShort(in);
-	h->max_clients = MSG_ReadByte(in);
-	h->encrypted = MSG_ReadByte(in);
-	MSG_ReadData(in, h->challenge, CHALLENGE_LEN);
+    h->key = MSG_ReadLong(in);
+    h->version = MSG_ReadLong(in);
+    h->port = MSG_ReadShort(in);
+    h->max_clients = MSG_ReadByte(in);
+    h->encrypted = MSG_ReadByte(in);
+    MSG_ReadData(in, h->challenge, CHALLENGE_LEN);
 }
 
 /**
@@ -80,20 +80,20 @@ void ParseHello(hello_t *h, msg_buffer_t *in)
  */
 void ParsePrint(q2_server_t *srv, msg_buffer_t *in)
 {
-	uint8_t level;
-	char *string;
+    uint8_t level;
+    char *string;
 
-	level = MSG_ReadByte(in);
-	string = MSG_ReadString(in);
+    level = MSG_ReadByte(in);
+    string = MSG_ReadString(in);
 
-	// obituary, parse for means of death
-	if (level == PRINT_MEDIUM) {
+    // obituary, parse for means of death
+    if (level == PRINT_MEDIUM) {
 
-	}
+    }
 
-	if (level == PRINT_CHAT) {
-		// log chat
-	}
+    if (level == PRINT_CHAT) {
+        // log chat
+    }
 }
 
 /**
@@ -102,40 +102,40 @@ void ParsePrint(q2_server_t *srv, msg_buffer_t *in)
  */
 void ParseCommand(q2_server_t *srv, msg_buffer_t *in)
 {
-	switch(MSG_ReadByte(in))
-	{
-	case CMD_COMMAND_TELEPORT:
-		ParseTeleport(srv, in);
-		break;
-	case CMD_COMMAND_INVITE:
-		ParseInvite(srv, in);
-		break;
-	case CMD_COMMAND_WHOIS:
-		ParseWhois(srv, in);
-		break;
-	}
+    switch(MSG_ReadByte(in))
+    {
+    case CMD_COMMAND_TELEPORT:
+        ParseTeleport(srv, in);
+        break;
+    case CMD_COMMAND_INVITE:
+        ParseInvite(srv, in);
+        break;
+    case CMD_COMMAND_WHOIS:
+        ParseWhois(srv, in);
+        break;
+    }
 }
 
 void ParseTeleport(q2_server_t *srv, msg_buffer_t *in)
 {
-	uint8_t client_id;
-	char *location;
-	char *reply;
-	q2_server_t *server, *found = 0;
+    uint8_t client_id;
+    char *location;
+    char *reply;
+    q2_server_t *server, *found = 0;
 
-	client_id = MSG_ReadByte(in);
-	location = MSG_ReadString(in);
+    client_id = MSG_ReadByte(in);
+    location = MSG_ReadString(in);
 
 
-	// player just issued teleport command with no arg, show available servers
-	if (!*location) {
-	    MSG_WriteByte(SCMD_SAYCLIENT, &srv->msg);
+    // player just issued teleport command with no arg, show available servers
+    if (!*location) {
+        MSG_WriteByte(SCMD_SAYCLIENT, &srv->msg);
         MSG_WriteByte(client_id, &srv->msg);
         MSG_WriteByte(PRINT_HIGH, &srv->msg);
         MSG_WriteString("Todo: show available servers\n", &srv->msg);
         SendBuffer(srv);
-	    return;
-	}
+        return;
+    }
 
     FOR_EACH_SERVER(server) {
         if (strcmp(server->name, location) == 0) {
@@ -143,19 +143,19 @@ void ParseTeleport(q2_server_t *srv, msg_buffer_t *in)
         }
     }
 
-	// do stuff
-	reply = va("sending you to %s:%d\n", found->ip, found->port);
-	char *stuff = va("sv !stuff CL %d connect %s:%d", client_id, found->ip, found->port);
+    // do stuff
+    reply = va("sending you to %s:%d\n", found->ip, found->port);
+    char *stuff = va("sv !stuff CL %d connect %s:%d", client_id, found->ip, found->port);
 
-	MSG_WriteByte(SCMD_SAYCLIENT, &srv->msg);
-	MSG_WriteByte(client_id, &srv->msg);
-	MSG_WriteByte(PRINT_HIGH, &srv->msg);
-	MSG_WriteString(reply, &srv->msg);
+    MSG_WriteByte(SCMD_SAYCLIENT, &srv->msg);
+    MSG_WriteByte(client_id, &srv->msg);
+    MSG_WriteByte(PRINT_HIGH, &srv->msg);
+    MSG_WriteString(reply, &srv->msg);
 
-	MSG_WriteByte(SCMD_COMMAND, &srv->msg);
-	MSG_WriteString(stuff, &srv->msg);
+    MSG_WriteByte(SCMD_COMMAND, &srv->msg);
+    MSG_WriteString(stuff, &srv->msg);
 
-	SendBuffer(srv);
+    SendBuffer(srv);
 }
 
 /**
@@ -163,44 +163,44 @@ void ParseTeleport(q2_server_t *srv, msg_buffer_t *in)
  */
 void ParseInvite(q2_server_t *srv, msg_buffer_t *in)
 {
-	q2_server_t *s;
-	uint8_t client_id;
-	char *text;
-	char message[MAX_STRING_CHARS];
+    q2_server_t *s;
+    uint8_t client_id;
+    char *text;
+    char message[MAX_STRING_CHARS];
 
-	client_id = MSG_ReadByte(in);
-	text = MSG_ReadString(in);
+    client_id = MSG_ReadByte(in);
+    text = MSG_ReadString(in);
 
-	// handle flood detection here
+    // handle flood detection here
 
-	if (text[0]) {
-		strncpy(message,
-			va("%s invites you to play on %s: %s",
-					srv->players[client_id].name,
-					srv->name,
-					text
-			),
-			sizeof(message)
-		);
-	} else {
-		strncpy(message,
-			va("You are cordially invited to join %s playing on %s",
-					srv->players[client_id].name,
-					srv->name
-			),
-			sizeof(message)
-		);
-	}
+    if (text[0]) {
+        strncpy(message,
+            va("%s invites you to play on %s: %s",
+                    srv->players[client_id].name,
+                    srv->name,
+                    text
+            ),
+            sizeof(message)
+        );
+    } else {
+        strncpy(message,
+            va("You are cordially invited to join %s playing on %s",
+                    srv->players[client_id].name,
+                    srv->name
+            ),
+            sizeof(message)
+        );
+    }
 
-	FOR_EACH_SERVER(s) {
-		if (!s->connected) {
-			continue;
-		}
+    FOR_EACH_SERVER(s) {
+        if (!s->connected) {
+            continue;
+        }
 
-		MSG_WriteByte(SCMD_SAYALL, &s->msg);
-		MSG_WriteString(message, &s->msg);
-		SendBuffer(s);
-	}
+        MSG_WriteByte(SCMD_SAYALL, &s->msg);
+        MSG_WriteString(message, &s->msg);
+        SendBuffer(s);
+    }
 }
 
 void ParseWhois(q2_server_t *srv, msg_buffer_t *in)
@@ -210,85 +210,85 @@ void ParseWhois(q2_server_t *srv, msg_buffer_t *in)
 
 void ParsePlayerConnect(q2_server_t *srv, msg_buffer_t *in)
 {
-	uint8_t client_id;
-	char *userinfo;
-	char *name;
-	q2_player_t *p;
+    uint8_t client_id;
+    char *userinfo;
+    char *name;
+    q2_player_t *p;
 
-	client_id = MSG_ReadByte(in);
-	userinfo = MSG_ReadString(in);
-	name = Info_ValueForKey(userinfo, "name");
+    client_id = MSG_ReadByte(in);
+    userinfo = MSG_ReadString(in);
+    name = Info_ValueForKey(userinfo, "name");
 
-	printf("%s just connected\n", name);
+    printf("%s just connected\n", name);
 
-	memset(&srv->players[client_id], 0, sizeof(q2_player_t));
-	p = &srv->players[client_id];
-	p->client_id = client_id;
-	strncpy(p->name, name, sizeof(p->name));
-	strncpy(p->userinfo, userinfo, sizeof(p->userinfo));
+    memset(&srv->players[client_id], 0, sizeof(q2_player_t));
+    p = &srv->players[client_id];
+    p->client_id = client_id;
+    strncpy(p->name, name, sizeof(p->name));
+    strncpy(p->userinfo, userinfo, sizeof(p->userinfo));
 
-	srv->playercount++;
+    srv->playercount++;
 }
 
 void ParsePlayerUpdate(q2_server_t *srv, msg_buffer_t *in)
 {
-	uint8_t client_id;
-	char *userinfo;
-	char *name;
-	q2_player_t *p;
+    uint8_t client_id;
+    char *userinfo;
+    char *name;
+    q2_player_t *p;
 
-	client_id = MSG_ReadByte(in);
-	userinfo = MSG_ReadString(in);
-	name = Info_ValueForKey(userinfo, "name");
+    client_id = MSG_ReadByte(in);
+    userinfo = MSG_ReadString(in);
+    name = Info_ValueForKey(userinfo, "name");
 
-	printf("%s updated\n", name);
+    printf("%s updated\n", name);
 
-	memset(&srv->players[client_id], 0, sizeof(q2_player_t));
-	p = &srv->players[client_id];
-	p->client_id = client_id;
-	strncpy(p->name, name, sizeof(p->name));
-	strncpy(p->userinfo, userinfo, sizeof(p->userinfo));
+    memset(&srv->players[client_id], 0, sizeof(q2_player_t));
+    p = &srv->players[client_id];
+    p->client_id = client_id;
+    strncpy(p->name, name, sizeof(p->name));
+    strncpy(p->userinfo, userinfo, sizeof(p->userinfo));
 }
 
 void ParsePlayerDisconnect(q2_server_t *srv, msg_buffer_t *in)
 {
-	uint8_t client_id = MSG_ReadByte(in);
+    uint8_t client_id = MSG_ReadByte(in);
 
-	printf("%d disconnected from %s\n", client_id, srv->name);
+    printf("%d disconnected from %s\n", client_id, srv->name);
 
-	memset(&srv->players[client_id], 0, sizeof(q2_player_t));
-	srv->playercount--;
+    memset(&srv->players[client_id], 0, sizeof(q2_player_t));
+    srv->playercount--;
 }
 
 void ParseMap(q2_server_t *srv, msg_buffer_t *in)
 {
-	char *map;
+    char *map;
 
-	map = MSG_ReadString(in);
-	strncpy(srv->map, map, sizeof(srv->map));
+    map = MSG_ReadString(in);
+    strncpy(srv->map, map, sizeof(srv->map));
 }
 
 void ParsePlayerList(q2_server_t *srv, msg_buffer_t *in)
 {
-	uint8_t i, client_id;
-	char *ui;
-	q2_player_t *p;
+    uint8_t i, client_id;
+    char *ui;
+    q2_player_t *p;
 
-	srv->playercount = MSG_ReadByte(in);
+    srv->playercount = MSG_ReadByte(in);
 
-	for (i=0; i<srv->playercount; i++) {
-		p = &srv->players[i];
-		i = MSG_ReadByte(in);
-		ui = MSG_ReadString(in);
+    for (i=0; i<srv->playercount; i++) {
+        p = &srv->players[i];
+        i = MSG_ReadByte(in);
+        ui = MSG_ReadString(in);
 
-		memset(p, 0, sizeof(q2_player_t));
+        memset(p, 0, sizeof(q2_player_t));
 
-		p->client_id = i;
-		strncpy(p->userinfo, ui, sizeof(p->userinfo));
-		strncpy(p->name, Info_ValueForKey(ui, "name"), sizeof(p->name));
+        p->client_id = i;
+        strncpy(p->userinfo, ui, sizeof(p->userinfo));
+        strncpy(p->name, Info_ValueForKey(ui, "name"), sizeof(p->name));
 
-		printf("Found %s\n", p->name);
-	}
+        printf("Found %s\n", p->name);
+    }
 }
 
 void ParseAuth(q2_server_t *q2, msg_buffer_t *in)
